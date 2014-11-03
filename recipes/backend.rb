a10503a8769945cb8f9f9c918a0d6daf481f51f2
John Bellone <john.bellone.jr@gmail.com>
2014-11-03 07:56:49 -0500

    Add python virtualenv for backend Django application.

--- recipes/backend.rb
+++ recipes/backend.rb
@@ -1,9 +1,8 @@
 include_recipe 'postgresql::client'
 
-node.default['python']['install_method'] = 'source'
 node.default['python']['version'] = '3.4.2'
 node.default['python']['checksum'] = '44a3c1ef1c7ca3e4fd25242af80ed72da941203cb4ed1a8c1b724d9078965dd8'
-include_recipe 'python::default'
+include_recipe 'python::source'
 
 root_path = ::File.join('/home', node['taiga']['user'], 'taiga-front')
 directory root_path do
@@ -19,4 +18,23 @@ git root_path do
   user node['taiga']['user']
   group node['taiga']['group']
   action :checkout
+end
+
+directory ::File.join(root_path, 'settings') do
+  owner node['taiga']['user']
+  group node['taiga']['group']
+  recursive true
+  not_if { ::Dir.exist?(::File.join(root_path, 'settings')) }
+end
+
+python_virtualenv root_path do
+  interpreter 'python3.4'
+  owner node['taiga']['user']
+  group node['taiga']['group']
+end
+
+python_pip '-r requirements.txt' do
+  virtualenv root_path
+  owner node['taiga']['user']
+  group node['taiga']['group']
 end
